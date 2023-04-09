39d9fba26c5f010949fa54400ec7eb1d96fc20af
amandaochan laptop <20667320+amandaochan@users.noreply.github.com>
2023-04-08 23:30:45 -0400

    R

---
+++ src/r_code/test.R
@@ -0,0 +1,20 @@
+# Install packages and load libraries
+# install.packages("htmltools")
+# library(htmltools)
+
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+# 
+# BiocManager::install("DESeq2")
+print("hello world")
+#library("DESeq2")
+#library(ggplot2)
+
+# Read in raw counts matrix
+# library(readr)
+# df <- read_tsv("C:\Users\Amanda Chan\Documents\440 Project\Dataset\GSE114065_raw_counts_GRCh38.p13_NCBI.tsv.gz")
+
+# Unzips gz file and writes over with unzipped version
+#R.utils::gunzip("/Users/Amanda Chan/Documents/440 Project/Dataset/GSE114065_raw_counts_GRCh38.p13_NCBI.tsv.gz")
+
+#countData <- read.table(file = '/Users/Amanda Chan/Documents/440 Project/Dataset/GSE114065_raw_counts_GRCh38.p13_NCBI.tsv', sep = '\t', header = TRUE)
